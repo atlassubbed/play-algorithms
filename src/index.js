a5acf4d19d06a786a87f6c09b54cae41ad6c106b
atlassubbed <atlassubbed@gmail.com>
2019-04-12 23:01:06 -0400

    adds implementation for escaping spaces in a string

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 module.exports = {
   ...require("./arrays/is-unique"),
-  ...require("./arrays/check-permutations")
+  ...require("./arrays/check-permutations"),
+  ...require("./arrays/urlify")
 }
